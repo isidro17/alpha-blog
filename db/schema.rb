460b8569060ffed596521d7d9dddec81ed4a3582
Isidro <isidro.jara17@gmail.com>
2017-06-21 12:35:32 +0000

    Add secure password an attribute to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621073753) do
+ActiveRecord::Schema.define(version: 20170621122835) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20170621073753) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
