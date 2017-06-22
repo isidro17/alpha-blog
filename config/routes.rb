1843703d513f102329c8055c4ee9d8cbfe13713e
Isidro <isidro.jara17@gmail.com>
2017-06-22 10:52:44 +0000

    Signup users from UI

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
   # get 'pages/about', to:'pages#about' -- Y hay que quitar este tambien, porque hemos quitado el anterior. En su lugar hay que poner
   get 'about', to:'pages#about'
   
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new] # Es equivalente a: post 'users', to: 'users#create'
+  
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
